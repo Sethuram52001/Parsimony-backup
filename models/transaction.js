23255beeb4df4d1c5d62fb24d87b4c6886e48479
Sethuram <sethuram52001@gmail.com>
2022-08-15 18:41:43 +0530

    Add transaction model

---
+++ models/transaction.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    transactionType: {
+      type: String,
+      enum: {
+        values: ['income', 'expense', 'transfer'],
+        message: '{VALUE} is not supported',
+      },
+      required: true,
+    },
+    amount: {
+      type: Number,
+      min: [0, 'Must be at least 0, got {VALUE}'],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Transaction = mongoose.model(
+  'Transaction',
+  transactionSchema,
+  'transactions',
+);
+module.exports = Transaction;
