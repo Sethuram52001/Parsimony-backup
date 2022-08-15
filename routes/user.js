11583568f017974ba90ce5806d03304de9d49bb0
Sethuram <sethuram52001@gmail.com>
2022-08-15 23:03:19 +0530

    Update user route to add new account to accounts list

--- routes/user.js
+++ routes/user.js
@@ -3,15 +3,40 @@ const router = express.Router();
 const User = require('../models/user');
 
 router.get('/user/:gmailId', async (req, res) => {
-  console.log('user route hit');
   const { gmailId } = req.params;
   try {
     const user = await User.findOne({
       email: gmailId,
     });
     if (user) {
-      console.log(user.accounts);
-      console.log(user.accounts.length);
+      return res.json({ status: 'success', user });
+    } else {
+      return res.json({ status: 'error', message: 'cannot find user' });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.json({ status: 'error', error });
+  }
+});
+
+router.put('/user/:gmailId', async (req, res) => {
+  const { gmailId } = req.params;
+  try {
+    const user = await User.findOne({
+      email: gmailId,
+    });
+    if (user) {
+      await User.updateOne(
+        {
+          email: gmailId,
+        },
+        {
+          accounts: [...user.accounts, req.body.account],
+        },
+        {
+          runValidators: true,
+        }
+      );
       return res.json({ status: 'success', user });
     } else {
       return res.json({ status: 'error', message: 'cannot find user' });
