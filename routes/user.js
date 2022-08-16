91f225299db42d0ba6b2ba0376fad1081a302905
Sethuram <sethuram52001@gmail.com>
2022-08-16 15:47:31 +0530

    Update response status and messages

--- routes/user.js
+++ routes/user.js
@@ -9,13 +9,15 @@ router.get('/user/:gmailId', async (req, res) => {
       email: gmailId,
     });
     if (user) {
-      return res.json({ status: 'success', user });
-    } else {
-      return res.json({ status: 'error', message: 'cannot find user' });
+      return res.status(200).json({ isError: false, user });
     }
+
+    return res
+      .status(404)
+      .json({ isError: true, message: 'Cannot find the user!' });
   } catch (error) {
     console.log(error);
-    return res.json({ status: 'error', error });
+    return res.status(400).json({ isError: true, error });
   }
 });
 
@@ -37,44 +39,57 @@ router.put('/user/:gmailId/add-account', async (req, res) => {
           runValidators: true,
         }
       );
-      return res.json({ status: 'success', user });
-    } else {
-      return res.json({ status: 'error', message: 'cannot find user' });
+      return res.status(200).json({
+        isError: false,
+        message: `Added new account: ${req.body.account.accountName} to the existing list`,
+      });
     }
+
+    return res
+      .status(404)
+      .json({ isError: true, message: 'Cannot find the user!' });
   } catch (error) {
     console.log(error);
-    return res.json({ status: 'error', error });
+    return res.status(400).json({ isError: true, error });
   }
 });
 
 router.put('/user/:gmailId/delete-account', async (req, res) => {
   const { gmailId } = req.params;
   try {
-    const user = await User.findOne({
-      email: gmailId,
-    });
-    if (user) {
-      const updatedAccounts = [...user.accounts].filter(
-        (account) => account.accountName !== req.body.account.accountName
-      );
-      await User.updateOne(
-        {
-          email: gmailId,
-        },
-        {
-          accounts: [...updatedAccounts],
-        },
-        {
-          runValidators: true,
-        }
-      );
-      return res.json({ status: 'success', user });
-    } else {
-      return res.json({ status: 'error', message: 'cannot find user' });
+    const accountToBeDeleted = req.body.account.accountName;
+    if (accountToBeDeleted && typeof accountToBeDeleted !== 'undefined') {
+      const user = await User.findOne({
+        email: gmailId,
+      });
+      if (user) {
+        const updatedAccounts = [...user.accounts].filter(
+          (account) => account.accountName !== accountToBeDeleted
+        );
+        await User.updateOne(
+          {
+            email: gmailId,
+          },
+          {
+            accounts: [...updatedAccounts],
+          },
+          {
+            runValidators: true,
+          }
+        );
+        return res.status(200).json({
+          isError: false,
+          message: `Deleted account: ${accountToBeDeleted} from the existing list`,
+        });
+      }
+      return res
+        .status(404)
+        .json({ isError: true, message: 'Cannot find the user!' });
     }
+    return res.status(400).json({ isError: true, message: 'Invalid account!' });
   } catch (error) {
     console.log(error);
-    return res.json({ status: 'error', error });
+    return res.status(400).json({ isError: true, error });
   }
 });
 
