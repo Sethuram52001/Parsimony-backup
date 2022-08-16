7a164fcbbd1758f8ceda8375117d14352ec89203
Sethuram <sethuram52001@gmail.com>
2022-08-16 16:34:56 +0530

    Introduce update and delete features in transaction route

--- routes/transaction.js
+++ routes/transaction.js
@@ -20,4 +20,47 @@ router.post('/transaction', async (req, res) => {
   }
 });
 
+router.put('/transaction/:transactionId', async (req, res) => {
+  const { transactionId } = req.params;
+  const { email, transactionType, amount } = req.body;
+  try {
+    await Transaction.updateOne(
+      {
+        _id: transactionId,
+      },
+      {
+        email,
+        transactionType,
+        amount,
+      },
+      {
+        runValidators: true,
+      }
+    );
+    return res.status(200).json({
+      isError: false,
+      message: 'Successfully updated the transaction record!',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ isError: true, error });
+  }
+});
+
+router.delete('/transaction/:transactionId', async (req, res) => {
+  const { transactionId } = req.params;
+  try {
+    await Transaction.deleteOne({
+      _id: transactionId,
+    });
+    return res.status(200).json({
+      isError: false,
+      message: 'Successfully deleted the transaction record!',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ isError: true, error });
+  }
+});
+
 module.exports = router;
