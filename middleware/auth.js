db800119835c06a813b03dc856d0788e618023a9
Sethuram <sethuram52001@gmail.com>
2022-08-21 14:00:49 +0530

    Made correction to parse header in a request

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 exports.auth = (req, res, next) => {
-  const token = req.header('x-auth-token');
+  const token = JSON.parse(req.header('x-auth-token'));
 
   if (!token) {
     return res
