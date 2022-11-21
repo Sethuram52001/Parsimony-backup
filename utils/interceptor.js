180db78eafc253826940cddd44a817fbffa675c4
Sethuram <sethuram52001@gmail.com>
2022-11-21 16:32:14 +0530

    Add utility function to mock response

---
+++ utils/interceptor.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mockResponse: () => {
+    const res = {};
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+    return res;
+  },
+};
