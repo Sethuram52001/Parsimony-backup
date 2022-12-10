7b04b8e11301af878a202c5ff5a289d347d8c51e
Sethuram <sethuram52001@gmail.com>
2022-12-10 13:28:29 +0530

    Redid dashboard page

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,12 +1,50 @@
-import * as React from 'react';
+import { Box, Typography } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2/Grid2';
+import AccountCard from '../components/AccountCard/AccountCard';
 import { DoughnetStat } from '../components/Statistics';
+import TransactionsList from '../components/Transaction/TransactionsList';
 
 const Dashboard = () => {
+  const accounts = [1, 2, 3];
   return (
-    <React.Fragment>
-      <h1>Dashboard</h1>
-      <DoughnetStat />
-    </React.Fragment>
+    <Grid sx={{ height: '100vh' }}>
+      <Grid container md={12}>
+        <Grid container xs={12}>
+          <Typography variant="h5">Your accounts</Typography>
+        </Grid>
+        <Grid container xs={12}>
+          {accounts.map((account, idx) => (
+            <AccountCard key={idx} />
+          ))}
+        </Grid>
+      </Grid>
+      <Grid container md={12}>
+        <Grid container xs={12}>
+          <Typography variant="h5">Expense graphic</Typography>
+        </Grid>
+        <Grid container xs={12}>
+          <DoughnetStat />
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid container xs={12} md={6}>
+          <Grid container xs={12}>
+            <Typography variant="h5">Your past transactions</Typography>
+          </Grid>
+          <Grid container xs={12}>
+            <TransactionsList />
+          </Grid>
+        </Grid>
+        <Grid container xs={12} md={6}>
+          <Grid container xs={12}>
+            <Typography variant="h5">Upcoming payments</Typography>
+          </Grid>
+          <Grid container xs={12}>
+            <TransactionsList />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
