001b62bc171a7f36d9472202fa81dcc4a21ef941
Sethuram <sethuram52001@gmail.com>
2022-12-19 21:34:13 +0530

    Change static to dynamic values

--- client/src/components/AccountCard/AccountCard.tsx
+++ client/src/components/AccountCard/AccountCard.tsx
@@ -1,11 +1,20 @@
 import { Card, CardContent, Typography } from '@mui/material';
+import { PropsWithChildren } from 'react';
 
-const AccountCard = () => {
+interface AccountCardProps {
+  _id: string;
+  accountName: string;
+  balance: Number;
+}
+
+const AccountCard = ({ accountName, balance }: AccountCardProps) => {
   return (
     <Card sx={{ backgroundColor: 'aqua', minWidth: '250px', m: '5px' }}>
       <CardContent>
-        <Typography variant="h5">account name</Typography>
-        <Typography variant="body2">balance: 1000</Typography>
+        <Typography variant="h5">account: {accountName}</Typography>
+        <Typography variant="body2">
+          balance: <>{balance}</>
+        </Typography>
       </CardContent>
     </Card>
   );
