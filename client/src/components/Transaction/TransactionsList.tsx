db3581860843e3af624dc57918b5422e1292e5f4
Sethuram <sethuram52001@gmail.com>
2022-12-20 22:17:54 +0530

    Move the data fetch logic to pages from component

--- client/src/components/Transaction/TransactionsList.tsx
+++ client/src/components/Transaction/TransactionsList.tsx
@@ -1,42 +1,35 @@
 import TransactionCard from './TransactionCard';
-import { default as api } from '../../store/apiSlice';
-import { useAppSelector as useSelector } from '../../hooks/hooks';
 import { LoadingTransactions } from '../Loading';
 
-const TransactionsList = () => {
-  const timeSpan = useSelector((state) => state.filter.timeSpan);
-  const date = useSelector((state) => state.filter.date);
-  let transactions;
-  const { data, isFetching, isSuccess, isError } =
-    api.useGetTransactionsByDateQuery({
-      timeSpan,
-      date,
-    });
-  if (isFetching) {
-    console.log('fetching');
-  } else if (isSuccess) {
-    const { transactions: tx } = data;
-    transactions = tx;
-  } else if (isError) {
-    console.log('error');
-  }
+interface Transaction {
+  _id: string;
+  transactionType: string;
+  account: string;
+  amount: number;
+  category: string;
+  description: string;
+  createdAt: string;
+}
 
+interface TransactionsListProps {
+  transactions: Transaction[];
+}
+
+const TransactionsList = (transactions: TransactionsListProps) => {
   return (
     <div style={{ marginLeft: '10px' }}>
-      {isFetching
-        ? Array.from(new Array(3)).map((item) => <LoadingTransactions />)
-        : transactions &&
-          transactions.map((transaction) => (
-            <TransactionCard
-              key={transaction._id}
-              transactionType={transaction.transactionType}
-              account={transaction.account}
-              amount={transaction.amount}
-              category={transaction.category}
-              description={transaction.description}
-              createdAt={transaction.createdAt}
-            />
-          ))}
+      {transactions &&
+        transactions.transactions.map((transaction: Transaction) => (
+          <TransactionCard
+            key={transaction._id}
+            transactionType={transaction.transactionType}
+            account={transaction.account}
+            amount={transaction.amount}
+            category={transaction.category}
+            description={transaction.description}
+            createdAt={transaction.createdAt}
+          />
+        ))}
     </div>
   );
 };
--- client/src/pages/Transactions.tsx
+++ client/src/pages/Transactions.tsx
@@ -1,12 +1,40 @@
 import * as React from 'react';
+import { default as api } from '../store/apiSlice';
+import { useAppSelector as useSelector } from '../hooks/hooks';
 import { TransactionsList, Filter } from '../components';
 
 const Transactions = () => {
+  const timeSpan = useSelector((state) => state.filter.timeSpan);
+  const date = useSelector((state) => state.filter.date);
+  const [transactions, setTransactions] = React.useState<any[]>([]);
+  const { data, isFetching, isSuccess, isError } =
+    api.useGetTransactionsByDateQuery({
+      timeSpan,
+      date,
+    });
+
+  if (isFetching) {
+    console.log('fetching');
+  } else if (isSuccess) {
+    const { transactions: tx } = data;
+    if (tx !== transactions) {
+      setTransactions(tx);
+    }
+    console.log(tx);
+    console.log(transactions);
+  } else if (isError) {
+    console.log('error');
+  }
+
   return (
     <React.Fragment>
       <h1>Transactions</h1>
       <Filter />
-      <TransactionsList />
+      {isSuccess ? (
+        transactions && <TransactionsList transactions={transactions} />
+      ) : (
+        <></>
+      )}
     </React.Fragment>
   );
 };
