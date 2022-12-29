e830ad6fa0dda9979f058b9f66d639bc1616095e
Sethuram <sethuram52001@gmail.com>
2022-12-29 22:20:00 +0530

    Add statistics route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const transaction = require('./routes/transaction');
 const user = require('./routes/user');
+const statistics = require('./routes/statistics');
 const app = express();
 
 require('dotenv').config();
@@ -10,5 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/transaction', transaction);
 app.use('/api/user', user);
+app.use('/api/statistics', statistics);
 
 module.exports = app;
